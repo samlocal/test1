493a7fe84a5ce0baa6c9216dfcadf2d1a8de02b4
samlocal <sameerndl28@gmail.com>
2025-05-24 10:57:21 +0530

    first commit

---
+++ src/main/java/com/branchesexample/branchesexample/BranchesexampleApplication.java
@@ -0,0 +1,13 @@
+package com.branchesexample.branchesexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchesexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchesexampleApplication.class, args);
+	}
+
+}
