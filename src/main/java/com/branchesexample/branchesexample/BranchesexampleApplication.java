a5e3aea8d23d38a8aaf26c8c6a4fa0fa2c23a89c
samlocal <sameerndl28@gmail.com>
2025-05-24 11:17:57 +0530

    bug-fix-1-fixed

--- src/main/java/com/branchesexample/branchesexample/BranchesexampleApplication.java
+++ src/main/java/com/branchesexample/branchesexample/BranchesexampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BranchesexampleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("fixed");
 		SpringApplication.run(BranchesexampleApplication.class, args);
 	}
 
